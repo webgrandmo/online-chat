229c050ff117b16dacecad6e1937d9401331e509
Andy Trofimov <webgrandmo@gmail.com>
2021-03-05 23:56:01 +0200

    Add server and socket

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+const socket = io();
+
+const form = document.getElementById('chat-form');
+
+socket.on('message', (message) => {
+	console.log(message);
+});
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const inputMsg = e.target.elements.msg.value;
+	console.log(inputMsg);
+});
---
+++ server.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+const PORT = 3000 || process.env.PORT;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+	console.log('New WS connection');
+
+	socket.emit('message', 'Welcome to the chat!');
+
+	// Broadcast them user connects
+	socket.broadcast.emit('message', 'A user joined the chat');
+
+	// Runs then user disconnects
+	socket.on('disconnect', () => {
+		io.emit('message', 'A user left the chat');
+	});
+});
+server.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
